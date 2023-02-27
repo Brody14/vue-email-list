998e7551106a659ecbbd3cc721b99f04a3dced65
Brody14 <stefania.spinas@gmail.com>
2023-02-27 15:17:34 +0100

    FEAT printing random email list

--- js/script.js
+++ js/script.js
@@ -1,11 +1,28 @@
-console.log('Quando inizi le API ma era meglio una IPA')
+//console.log("Quando inizi le API ma era meglio una IPA");
 
-const { createApp } = Vue
+const { createApp } = Vue;
 
-  createApp({
-    data() {
-      return {
-    
-      }
+createApp({
+	data() {
+		return {
+			emailList: [],
+		};
+	},
+	methods: {
+		fetchEmail() {
+			axios
+				.get("https://flynn.boolean.careers/exercises/api/random/mail")
+				.then((res) => {
+					//console.log(res.data.response);
+          const email = res.data.response
+          this.emailList.push(email)
+				});
+		},
+	},
+  mounted(){
+    for(let i = 0; i < 10; i++) {
+      this.fetchEmail()
     }
-  }).mount('#app')
+    console.log(this.emailList)
+  }
+}).mount("#app");
